20eb2ba25d987e0993b75121a9472040c3d90a9a
rentainhe <596106517@qq.com>
2024-08-02 21:10:28 +0800

    refine video file

--- video_utils.py
+++ video_utils.py
@@ -2,34 +2,34 @@ import cv2
 import os
 from tqdm import tqdm
 
-def create_video_from_images(image_folder, output_video_path, frame_rate=30):
-    # 定义允许的图像后缀
+def create_video_from_images(image_folder, output_video_path, frame_rate=25):
+    # define valid extension
     valid_extensions = [".jpg", ".jpeg", ".JPG", ".JPEG"]
     
-    # 获取图像文件列表
+    # get all image files in the folder
     image_files = [f for f in os.listdir(image_folder) 
                    if os.path.splitext(f)[1] in valid_extensions]
-    image_files.sort()  # 排序，确保按正确的顺序读取图像
+    image_files.sort()  # sort the files in alphabetical order
     print(image_files)
     if not image_files:
         raise ValueError("No valid image files found in the specified folder.")
     
-    # 读取第一张图像以获取视频尺寸
+    # load the first image to get the dimensions of the video
     first_image_path = os.path.join(image_folder, image_files[0])
     first_image = cv2.imread(first_image_path)
     height, width, _ = first_image.shape
     
-    # 创建视频写入对象
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 可以选择其他编码方式，如 'XVID'
+    # create a video writer
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') # codec for saving the video
     video_writer = cv2.VideoWriter(output_video_path, fourcc, frame_rate, (width, height))
     
-    # 逐帧写入视频
+    # write each image to the video
     for image_file in tqdm(image_files):
         image_path = os.path.join(image_folder, image_file)
         image = cv2.imread(image_path)
         video_writer.write(image)
     
-    # 释放资源
+    # source release
     video_writer.release()
     print(f"Video saved at {output_video_path}")
 
